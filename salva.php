c3f77c892eae35c6cfd5af424215a366d6091420
Heitor <heitor170@gmail.com>
2019-10-18 13:28:37 -0300

    adicionando redirecionamento caso ocorra erro

--- salva.php
+++ salva.php
@@ -45,10 +45,13 @@ include("emails/defineNomeDoGrupoDeEmail.php");
 		if($execQuery == ""){
 			echo("<h2>Ocorreu um erro durante a inserção na tabela! Verifique se há algum espaço em branco e/ou se o E-Mail digitado é Unico</h2>");
 		}else{
-			//echo("Dados inseridos com sucesso");
-			header("Location: http://localhost/RHCompasso/telas/menuPrincipal.php");
+						header("Location: http://localhost/RHCompasso/telas/menuPrincipal.php");
 		}
 
 
-
-?>
+		?>
+		<?php 
+			
+		header("Refresh:1; url= http://localhost/RHCompasso/telas/menuPrincipal.php");
+		
+		?>
