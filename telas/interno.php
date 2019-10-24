d6d5b9d19f2009bf560314b2ca6cce06715529b7
Flávio Luis Milani <flavioluismilani@hotmail.com>
2019-10-24 08:59:49 -0300

    Organizando o código

--- telas/interno.php
+++ telas/interno.php
@@ -41,49 +41,8 @@ $campoV = 'class="txtVazio" ';
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt">
-
-<head>
-    <meta charset="UTF-8">
-    <title>RH Contratações</title>
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/arquivo.css">
-    <link rel="stylesheet" href="../css/menuPrincipal.css">
-
-
-
-</head>
-
-<body>
-    <header class="site-header">
-        <img src="http://www.compasso.com.br/wp-content/uploads/2018/04/Logo_Compasso_01-mini.png" alt="Compasso Tecnologia">
-        <nav>
-            <a class='nav inicio' href='menuPrincipal.php'>Início</a>
-            <div class="dropdown">
-            <a class="dropbtn nav">Emails <span class='caret'></span></a>
-            <div class="dropdown-content">
-                <a href='../emails/body-email/admissaoPOA.php?id=<?php echo $id?>'>5. Documentos Admissão POA</a>
-                <a href='../emails/body-email/admissaoRG.php?id=<?php echo $id?>'>5.1 Documentos Admissão RG</a>
-                <a href='../emails/body-email/admissaoPF.php?id=<?php echo $id?>'>5.2 Documentos de Admissão PF</a>
-                <a href='../emails/body-email/admissaoERE.php?id=<?php echo $id?>'>5.3 Documentos de Admissão ERE</a>
-                <a href='../emails/body-email/admissaoCWB.php?id=<?php echo $id?>'>5.4 Documentos de Admissão CWB</a>
-                <a href='../emails/body-email/admissaoSP_RJ.php?id=<?php echo $id?>'>5.5 Documentos de Admissão SP e RJ</a>
-                <a href='../emails/body-email/admissaoFNL.php?id=<?php echo $id?>'>5.6 Documentos de Admissão FNL</a>
-                <a href='../emails/body-email/admissaoRecife.php?id=<?php echo $id?>'>5.7 Documentos de Admissão Recife</a>
-                <a href='../emails/body-email/primeiro-alerta.php?id=<?php echo $id?>'>7. ALERTA - 1ª Experiência expira em 20 dias</a>
-                <a href='../emails/body-email/segundo-alerta.php?id=<?php echo $id?>'>7.1 ALERTA - 2ª Experiência expira em 20 dias</a>
-                <a href='../emails/body-email/novo-acesso.php?id=<?php echo $id?>'>8. Novo Acesso</a>
-                <a href='../emails/body-email/acesso-liberado.php?id=<?php echo $id?>>'>9. Acessos Liberado</a>
-                </div>
-            </div>
-            </div>
-            <a class='nav filter last' href='../login/user/sair.php'>Sair</a>
-        </nav>
-
-    </header>
+    <?php echo file_get_contents("telasHeader.html"); ?>
+    
     <main>
         <section class='menu-inicial'>
             <h2 id='nome'>Interno</h2>
