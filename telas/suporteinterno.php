d893ae69091ce94114a50d29c06a434a8f5ae408
Flávio Luis Milani <flavioluismilani@hotmail.com>
2019-11-01 11:00:32 -0300

    Verificação E-mail

--- telas/suporteinterno.php
+++ telas/suporteinterno.php
@@ -238,6 +238,9 @@ $emailsoli = buscavias($conn, $id);
                                                                                                 if(buscasuporteExiste($conn, $rows_dados['USUARIO']) > 1) {
                                                                                                     $usuario_atv = "USUÁRIO E/OU E-MAIL JÁ EXISTE";
                                                                                                     echo ($campoV);                                                                                                    
+                                                                                                } if(buscasuporteExiste($conn, $rows_dados['EMAIL_SUP']) > 1) {
+                                                                                                    $usuario_atv = "USUÁRIO E/OU E-MAIL JÁ EXISTE";
+                                                                                                    echo ($campoV);                                                                                                    
                                                                                                 } ?>> <?php echo $usuario_atv ?> </td>
                             <td <?php if ($rows_dados['EMAIL_SUP'] == "") {
                                         echo ($campoV);
