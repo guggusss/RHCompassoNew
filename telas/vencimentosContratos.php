62d51813a7eb70b4674d88832356ba65e6526298
carloszambiasi <carlos.zambiasi_est@compasso.com.br>
2019-11-05 16:00:22 -0300

    tela vencimentoContratos posição das legendas ficou errada

--- telas/vencimentosContratos.php
+++ telas/vencimentosContratos.php
@@ -274,9 +274,9 @@ $translado = buscasuporte($conn, $id);
         </section>
         <h3>Se o campo Data Admissão foi alterado, clique para aplicar as modificações:
         <td><input type="button" class="btn btn-default" value="Recarregar" onClick="history.go(0)"></td>
-        <?php echo file_get_contents("telasLegendas.html"); ?>
     </main>
     <footer>
+        <?php echo file_get_contents("telasLegendas.html"); ?>
         <h2></h2>
     </footer>
     <script src="../js/jquery.js"></script>
