268ab99912e27eb6ef50be00cc2653e45c84b3a1
carloszambiasi <carlos.zambiasi_est@compasso.com.br>
2019-11-05 15:05:06 -0300

    teste1

--- emails/body-email/admissaoRecife.php
+++ emails/body-email/admissaoRecife.php
@@ -70,7 +70,7 @@ $NewDate =  date_format($dataF, 'd/m/Y');
               <p  class="p2text">
                 <span class="spanText">Excel</span></p>
             </td>
-            <td nowrap="" class="tdStyle4">
+            <td nowrap="" style="width:80.55pt;height:10.4pt;padding:0 3.5pt;border-style:none solid solid none;border-right-width:1pt;border-bottom-width:1pt;border-right-color:gray;border-bottom-color:gray;">
               <p  style="text-align:center;"><span class="spanText">NÃO</span></p>
             </td>
             <td valign="bottom" nowrap="" class="tdStyle3">
@@ -86,7 +86,7 @@ $NewDate =  date_format($dataF, 'd/m/Y');
               <p  class="p2text">
                 <span class="spanText">.pdf</span></p>
             </td>
-            <td nowrap="" class="tdStyle4">
+            <td nowrap="" style="width:80.55pt;height:10.4pt;padding:0 3.5pt;border-style:none solid solid none;border-right-width:1pt;border-bottom-width:1pt;border-right-color:gray;border-bottom-color:gray;">
               <p  class="p2text">
                 <span class="spanText">NÃO</span></p>
             </td>
@@ -440,7 +440,7 @@ $NewDate =  date_format($dataF, 'd/m/Y');
               <p  class="p2text">
                 <span class="spanText">.pdf</span></p>
             </td>
-            <td nowrap="" class="tdStyle4">
+            <td nowrap="" style="width:80.55pt;height:10.4pt;padding:0 3.5pt;border-style:none solid solid none;border-right-width:1pt;border-bottom-width:1pt;border-right-color:gray;border-bottom-color:gray;">
               <p  style="text-align:center;"><b><span class="spanText">SIM</span></b></p>
 =======
           <tr style="height:10.4pt;">
