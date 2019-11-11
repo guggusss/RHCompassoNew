6b52206c1eaa4306e11553dfecf8eb906309610e
carloszambiasi <carlos.zambiasi_est@compasso.com.br>
2019-11-11 15:29:04 -0300

    comitando dnv o copy funcionando

--- js/seleciona.js
+++ js/seleciona.js
@@ -1,8 +1,4 @@
 document.querySelector("#foo").onclick = function() {
-    var divACopiar = document.querySelector("#selecionaPagina");
-
-    var range = document.createRange();
-    range.selectNode(divACopiar);
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
+    document.execCommand('selectAll',false,null);
+    document.execCommand("copy")
 };
