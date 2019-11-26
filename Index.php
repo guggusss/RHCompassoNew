4e1ff3f20d217a9a891fbb00427274020600a2cd
Heitor <heitor170@gmail.com>
2019-11-26 14:57:06 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt">
-        <head>
-                <meta charset="utf-8"/>
-                <title>Redirecionando...</title>             
-                <meta http-equiv="refresh" content="0.1; URL='telas/Index.php'"/>
-            </head>
+<meta http-equiv="refresh" content="0.1; URL='telas/Index.php'"/>
+          
 
