69fe403e83d2b3e353d6c829c6f0210b082bc571
HeItOr <hei-tor9@hotmail.com>
2020-01-28 09:07:58 -0300

    rerm. cod

--- telas/suporteinterno.php
+++ telas/suporteinterno.php
@@ -123,24 +123,7 @@ $campoV = 'class="txtVazio" ';
                         <thead>
                         <tbody>
                             <tr>
-                                <?php while ($rows_dados = mysqli_fetch_assoc($resultadoBarr)) {  ?>
-                                    <th width='100px'><?php echo $rows_dados['STATUS']; ?></th>
-                                    <th width='100px'><?php echo $rows_dados['NOME']; ?></th>
-                                    <th width='170px'><?php echo $rows_dados['DATA_ADMISSAO']; ?></th>
-                                    <th width='170px'><?php if($rows_dados['ID_SEDE'] == "1"){echo "CWB";} 
-                                                            if($rows_dados['ID_SEDE'] == "2"){echo "ERE";}
-                                                            if($rows_dados['ID_SEDE'] == "3"){echo "PF";}
-                                                            if($rows_dados['ID_SEDE'] == "4"){echo "POA";}
-                                                            if($rows_dados['ID_SEDE'] == "5"){echo "RG";}
-                                                            if($rows_dados['ID_SEDE'] == "6"){echo "SP";}
-                                                            if($rows_dados['ID_SEDE'] == "7"){echo "FLN";}
-                                                            if($rows_dados['ID_SEDE'] == "8"){echo "XAP";}
-                                                            if($rows_dados['ID_SEDE'] == "9"){echo "REC";}
-                                                            if($rows_dados['ID_SEDE'] == "10"){echo "CAX";}
-                                                            if($rows_dados['ID_SEDE'] == "11"){echo "RJ";}
-                                                            if($rows_dados['ID_SEDE'] == "12"){echo "JAG";}
-                                                            if($rows_dados['ID_SEDE'] == "13"){echo "BH";}?></th>
-                                <?php  } ?>
+                                <?php include("extensao.php"); ?>
                             </tr>
                         </tbody>
                 </table>
