18322a17ae32a2557f0b4fe1461ee3c845c2d452
Flávio Luis Milani <flavioluismilani@hotmail.com>
2020-01-28 10:37:40 -0300

    Erro salvo em excel

--- salva.php
+++ salva.php
@@ -37,8 +37,12 @@ $sql = "INSERT INTO `admissao_dominio` ( `STATUS`,`ID_SEDE`,`ID_TIPO`,`ID_CAPTAC
 
 $execQuery = mysqli_query($conn, $sql);
 
-if ($execQuery == "") {
-	$msg = mysqli_error($conn);?>
+if ($execQuery == "") {		
+	$msg = mysqli_error($conn);
+	$output .= '';
+	header('Content-Type: application/xls');
+	header('Content-Disposition: attachment; filename=LogErro.xls');
+	echo $output;?>
 	<head>
 		<meta charset="UTF-8">
 		<title>RH Contratações</title>
